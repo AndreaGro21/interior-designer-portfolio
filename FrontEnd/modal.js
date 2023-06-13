e1973e9bbf42f7f5bc809d141164e5e82a151430
andrea gronchi <andreagronchi21@gmail.com>
2023-06-13 13:13:46 +0200

    second dataform commit

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -110,20 +110,14 @@ document.getElementById('close-btn').addEventListener(
 
 )
 
-function closeModal() {
-    /*   document.querySelector("#my-modal").style.display = "none" */
-}
-
-
 function addImg() {
     const formEle = document.querySelector("#modal2")
     formEle.addEventListener("submit", a => {
         a.preventDefault();
-        const dataForm = new FormData( );
-       
+        const dataForm = new FormData(formEle );
 
         /*     const data = Object.fromEntries(dataForm);
-         const imgLoader = document.querySelector("#image")
+        const imgLoader = document.querySelector("#image")
             const imgFile = imgLoader.files[0];
             data.image = imgFile;
             let  postNewImg = {
@@ -134,18 +128,21 @@ function addImg() {
             console.log(postNewImg) */
 
             var image = document.getElementById("image").files[0]
+            console.log(image)
             var title = document.getElementById("title").value
-            var category = document.getElementById(category)
-        dataForm.append('file', image)
-        dataForm.append("title", title,)
-        dataForm.append("categoryId", category,)
+            console.log(title)
+            var category = document.querySelector("#category").value
+            console.log(category)
+        dataForm.append("file", image)
+        dataForm.append("title", title)
+        dataForm.append("categoryId", category)
         console.log(dataForm)
         let token = sessionStorage.getItem('reponseLogin')
         token = JSON.parse(token)
         let getToken = token.token;
-        async function pushNewImg() {
+        
             try {
-                await fetch("http://localhost:5678/api/works", {
+                fetch("http://localhost:5678/api/works/", {
                     method: 'POST',
                     headers: {
                        /*  'Content-Type': 'multipart/form-data', */
@@ -156,8 +153,8 @@ function addImg() {
             } catch (error) {
                 console.error('Error:', error);
             }
-        }
-        pushNewImg()
+        
+       
     })
 }
 addImg()
